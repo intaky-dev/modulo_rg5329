6627cc57a52a36597133913cfff52a90bba93db3
intaky-dev <kazapeke@gmail.com>
2025-08-15 15:43:05 -0300

    random

--- rg5329_tax_module/__manifest__.py
+++ rg5329_tax_module/__manifest__.py
@@ -26,6 +26,7 @@
     "data": [
         "security/ir.model.access.csv",
         "data/tax_data.xml",
+        "views/product_template_views.xml",
         "views/res_partner_views.xml",
         "views/account_tax_views.xml"
     ],
